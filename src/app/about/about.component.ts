98bb4b34150cfb91894104069f5302e596f0a7af
Eslam-mohamed <em354219@gmail.com>
2024-02-03 03:03:23 +0200

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,150 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+
+
+
+export class AboutComponent {
+  
+  obj1: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj2: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  obj3: Prodects = {
+    img: "../../assets/6.jpg",
+    title: "Watch",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 2000
+  }
+  obj4: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj5: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  obj6: Prodects = {
+    img: "../../assets/6.jpg",
+    title: "Watch",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 2000
+  }
+  obj7: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj8: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  obj9: Prodects = {
+    img: "../../assets/6.jpg",
+    title: "Watch",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 2000
+  }
+  obj10: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj11: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  obj12: Prodects = {
+    img: "../../assets/6.jpg",
+    title: "Watch",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 2000
+  }
+  obj13: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj14: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  obj15: Prodects = {
+    img: "../../assets/6.jpg",
+    title: "Watch",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 2000
+  }
+  obj16: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj17: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  obj18: Prodects = {
+    img: "../../assets/6.jpg",
+    title: "Watch",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 2000
+  }
+  obj19: Prodects = {
+    img: "../../assets/5.jpg",
+    title: "Card",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 20 
+  }
+  obj20: Prodects = {
+    img: "../../assets/4.jpg",
+    title: "Shoes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis praesentium ipsam voluptatibus asperiores veritatis minima vero delectus, eveniet error provident quod quae quidem, adipisci ab! Dolores rerum tenetur dignissimos.",
+    price: 300 
+  }
+  prodects = [this.obj1, this.obj2, this.obj3, this.obj4, this.obj5,
+    this.obj6, this.obj7, this.obj8, this.obj9, this.obj10, this.obj11,
+    this.obj12, this.obj13, this.obj14, this.obj15, this.obj16,
+    this.obj17, this.obj18, this.obj19, this.obj20];
+}
+
+interface Prodects {
+  img: string;
+  title: string;
+  desc: string;
+  price: number;
+}
+
+let prodects: Prodects[];
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbaarComponent } from './navbaar/navbaar.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    NotFoundComponent,
+    NavbaarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
